acb80fc06238ee60654f6200f5b6a8a6017dd3cc
Stefan Turner <turner.stefan281@gmail.com>
2022-01-09 16:27:36 +0100

    add get ready animation

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,14 @@ const slideOutElements = document.querySelectorAll('.slide-out');
 const highscoreList = document.querySelector('.highscores');
 
 
+
 scoresBtn.addEventListener('click', (e) => {
     slideOutElements.forEach(elem => elem.classList.toggle('slide-out--active'));
     highscoreList.classList.toggle('highscores--open');
     e.target.innerHTML === 'Open Scoreboard' ? e.target.innerHTML = 'Close Scoreboard' : e.target.innerHTML = 'Open Scoreboard';    
+});
+
+
+document.querySelector('.startpage__play').addEventListener('click', () => {
+    document.querySelector('.ready').classList.add('ready--open');
 });
