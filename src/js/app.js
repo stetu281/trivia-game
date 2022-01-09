b4baabb65a562269b8b60d4f5b7d644f401698a2
Stefan Turner <turner.stefan281@gmail.com>
2022-01-09 17:12:18 +0100

    add gamepage

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import css from '../scss/main.scss';
 const scoresBtn = document.querySelector('.highscores__btn');
 const slideOutElements = document.querySelectorAll('.slide-out');
 const highscoreList = document.querySelector('.highscores');
+const startpage = document.querySelector('.startpage');
+const gamepage = document.querySelector('.gamepage');
+const getReadypage = document.querySelector('.ready');
 
 
 
@@ -14,5 +17,10 @@ scoresBtn.addEventListener('click', (e) => {
 
 
 document.querySelector('.startpage__play').addEventListener('click', () => {
-    document.querySelector('.ready').classList.add('ready--open');
+    getReadypage.classList.add('ready--open');
+    startpage.classList.add('startpage--fade-out');
+    setTimeout(() => {
+        gamepage.classList.add('gamepage--open')
+        getReadypage.classList.remove('ready--open');
+    }, 5000);
 });
